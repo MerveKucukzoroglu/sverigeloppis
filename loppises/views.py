b4a5f7f53461fda0afd86d87bd58c949769320b9
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-05-29 12:49:59 +0000

    Add search functionality

--- loppises/views.py
+++ loppises/views.py
@@ -1,13 +1,28 @@
 """ Loppis app views file """
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Loppis
 
 def all_loppises(request):
     """ A view to return list of all the loppises """
+
     loppises = Loppis.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('loppises'))
+            
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            loppises = loppises.filter(queries)
 
     context = {
         'loppises': loppises,
+        'search_term': query,
     }
     return render(request, 'loppises/loppises.html', context)
 
