1b9d7eaa35a224974e352fb6ae83bddb4dacf1ef
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-05-29 08:59:41 +0000

    Customize admin

--- loppises/admin.py
+++ loppises/admin.py
@@ -4,5 +4,26 @@ from .models import Loppis, County
 
 # Register your models here.
 
-admin.site.register(Loppis)
-admin.site.register(County)
+
+class LoppisAdmin(admin.ModelAdmin):
+    list_display = (
+        'seller',
+        'county',
+        'country',
+        'created_on',
+        'start_date',
+        'end_date',
+    )
+
+    ordering = ('created_on',)
+
+
+class CountyAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'county',
+    )
+
+
+admin.site.register(Loppis, LoppisAdmin)
+admin.site.register(County, CountyAdmin)
