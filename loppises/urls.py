c909437c811a490636d8d062b561e08aac817cb3
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-05-29 09:08:36 +0000

    Add Loppises views and templates

---
+++ loppises/urls.py
@@ -0,0 +1,7 @@
+""" Loppis App urls file """
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_loppises, name='loppises'),
+]
--- loppises/views.py
+++ loppises/views.py
@@ -1,3 +1,12 @@
+""" Loppis app views file """
 from django.shortcuts import render
+from .models import Loppis
 
-# Create your views here.
+def all_loppises(request):
+    """ A view to return list of all the loppises """
+    loppises = Loppis.objects.all()
+
+    context = {
+        'loppises': loppises,
+    }
+    return render(request, 'loppises/loppises.html', context)
