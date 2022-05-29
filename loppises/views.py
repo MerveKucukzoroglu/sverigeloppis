34849782b9849f23df039f330fea612100599e72
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-05-29 16:11:35 +0000

    Add sortkey to loppises

--- loppises/views.py
+++ loppises/views.py
@@ -9,8 +9,24 @@ def all_loppises(request):
 
     loppises = Loppis.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'county':
+                sortkey = 'lower_name'
+                loppises = loppises.annotate(lower_name=Lower('county'))
+            if sortkey == 'county':
+                sortkey = 'county__county'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            loppises = loppises.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -20,10 +36,14 @@ def all_loppises(request):
             queries = Q(title__icontains=query) | Q(description__icontains=query) | Q(county__county__icontains=query) 
             loppises = loppises.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'loppises': loppises,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'loppises/loppises.html', context)
 
 
