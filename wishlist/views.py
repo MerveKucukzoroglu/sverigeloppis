7f55f84e54c015aab5f8679ad9e3f071ebf7c5db
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-04 14:53:20 +0000

    Add additional toast messages

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from loppises.models import Loppis
@@ -13,13 +13,13 @@ def view_wishlist(request):
 def add_to_wishlist(request, item_id):
     """ Add loppis item to wishlist """
 
-    loppis = Loppis.objects.get(pk=item_id)
+    loppis = get_object_or_404(Loppis, pk=item_id)
     quantity = request.POST.get('quantity')
     redirect_url = request.POST.get('redirect_url')
     wishlist = request.session.get('wishlist', {})
 
     wishlist[item_id] = quantity
-    messages.success(request, f'Added {loppis.title} to your wishlist')
+    messages.success(request, f'Added {loppis.title} to wishlist')
 
     request.session['wishlist'] = wishlist
     return redirect(redirect_url)
@@ -29,11 +29,15 @@ def remove_from_wishlist(request, item_id):
     """Remove the item from the wishlist"""
 
     try:
+        loppis = get_object_or_404(Loppis, pk=item_id)
         wishlist = request.session.get('wishlist', {})
         wishlist.pop(item_id)
+        messages.success(request, f'Removed {loppis.title} from wishlist')
+
 
         request.session['wishlist'] = wishlist
         return HttpResponse(status=200)
     
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
