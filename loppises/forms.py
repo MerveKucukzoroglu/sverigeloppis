aca15c5d3b31321b29b04de41ec34b6a1a460bc4
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-05 12:26:51 +0000

    Create loppis form to add loppises

---
+++ loppises/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Loppis, County
+
+
+class LoppisForm(forms.ModelForm):
+
+    class Meta:
+        model = Loppis
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        counties = County.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in counties]
+
+        self.fields['county'].choices = friendly_names
+        for field_name, field in self.fields.itmes():
+            field.widget.attrs['class'] = 'border-dark rounded-0'
