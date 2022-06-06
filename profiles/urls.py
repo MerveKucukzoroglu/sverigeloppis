00c58cea1302a708710a2198625df830db89a129
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-06 20:04:38 +0000

    Create manage my loppis and contition with user authentication

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('my_loppises/', views.my_loppises, name='my_loppises'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from loppises.models import Loppis
 
 
 def profile(request):
@@ -8,3 +9,9 @@ def profile(request):
     context = {}
 
     return render(request, template, context)
+
+def my_loppises(request):
+    """Authenticated user views their own poems"""
+    logged_in_user = request.user
+    logged_in_user_loppises = Loppis.objects.filter(seller=logged_in_user)
+    return render(request, 'profiles/my_loppises.html', {'loppises': logged_in_user_loppises})
