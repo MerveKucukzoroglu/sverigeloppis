3aa92cd436a81c611915802e68f587c279c3d511
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-06 14:31:03 +0000

    Add django date/time picker & other form fields

--- loppises/forms.py
+++ loppises/forms.py
@@ -2,10 +2,25 @@ from django import forms
 from .models import Loppis, County
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
+
 class LoppisForm(forms.ModelForm):
 
     class Meta:
         model = Loppis
+        widgets = {
+            'start_date': DateInput(),
+            'end_date': DateInput(),
+            'start_time': TimeInput(),
+            'end_time': TimeInput(),
+        }
+        
         fields = (
             'title', 'start_date', 'end_date', 
             'start_time', 'end_time', 'country', 
@@ -21,4 +36,11 @@ class LoppisForm(forms.ModelForm):
 
         self.fields['county'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-dark rounded-0'
+            field.widget.attrs['class'] = 'border-dark rounded-0'  
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     start_date = cleaned_data.get("start_date")
+    #     end_date = cleaned_data.get("end_date")
+    #     if end_date < start_date:
+    #         raise forms.ValidationError("End date should be greater than start date.")
