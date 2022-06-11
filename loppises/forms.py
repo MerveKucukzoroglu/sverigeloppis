62ccf221a822c1cda0e448904744a303e463ecd4
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-11 10:50:00 +0000

    Force fill the country field as Sweden

--- loppises/forms.py
+++ loppises/forms.py
@@ -23,27 +23,22 @@ class LoppisForm(forms.ModelForm):
         }
         
         fields = (
-            'title', 'start_date', 'end_date', 
-            'start_time', 'end_time', 'country', 
-            'postcode', 'county', 'city', 
-            'street_address', 'phone_number', 
+            'title', 'start_date', 'end_date',
+            'start_time', 'end_time', 'country',
+            'postcode', 'county', 'city',
+            'street_address', 'phone_number',
             'image', 'description',
             )
-        
+
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         counties = County.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in counties]
-        
+
         self.fields['county'].choices = friendly_names
+        self.fields['country'].widget.attrs['placeholder'] = 'Sweden'
+        self.fields['country'].widget.attrs['readonly'] = True
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-dark rounded-0'
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     start_date = cleaned_data.get("start_date")
-    #     end_date = cleaned_data.get("end_date")
-    #     if end_date < start_date:
-    #         raise forms.ValidationError("End date should be greater than start date.")
