ab0370d554d3e1702b201fa06e936436bd67891c
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-11 07:04:52 +0000

    Add image processor to settings and render default img

--- sverige_loppis/settings.py
+++ sverige_loppis/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'wishlist.contexts.wishlist_contents',
             ],
             'builtins': [
