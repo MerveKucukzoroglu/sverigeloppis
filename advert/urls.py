48d9f25ff45be0adf616afb76e7de9fe16e66858
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-12 07:40:43 +0000

    Add class methods to webhook handler and webhook view

--- advert/urls.py
+++ advert/urls.py
@@ -1,8 +1,10 @@
 """ Advert App urls file """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.advert, name='advert'),
     path('advert_success/', views.advert_success, name='advert_success'),
+    path('wh/', webhook, name='webhook'),
 ]
--- advert/webhook_handler.py
+++ advert/webhook_handler.py
@@ -7,9 +7,28 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
+
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+        
+    
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
