125113019b049f9a55c3ccf2d6ead8716a3825f2
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-18 11:32:55 +0000

    Send confirmation email for newsletter subscriptions

--- subscription/views.py
+++ subscription/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .forms import NewsletterForm
 from django.contrib import messages
 from .models import Subscriptions
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 
 def subscribe(request):
@@ -13,6 +16,23 @@ def subscribe(request):
             email = newsletter_form.cleaned_data.get("email")
             if email:
                 messages.success(request, 'Successfully subscribed to our Newsletter!')
+                subscriber_email = email
+                subject = render_to_string(
+                        'subscription/confirmation_emails/confirmation_email_subject.txt',
+                        {'email': email})
+                body = render_to_string(
+                        'subscription/confirmation_emails/confirmation_email_body.txt',
+                        {
+                            'email': email,
+                            'subscriber_email': subscriber_email,
+                            'contact_email': settings.DEFAULT_FROM_EMAIL
+                        })
+                send_mail(
+                    subject,
+                    body,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [subscriber_email]
+                )
             else:
                 messages.error(request, 'This email already exists!')
             newsletter_form.save()
