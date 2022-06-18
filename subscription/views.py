cb0182fa3a54dfe079980176251b342036e4d46d
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-18 09:06:06 +0000

    Add if requested method is post condition to subscription form

--- subscription/views.py
+++ subscription/views.py
@@ -8,16 +8,17 @@ def subscribe(request):
     """ A view to show individual loppis details """
     newsletter_form = NewsletterForm(data=request.POST)
     
-    if newsletter_form.is_valid():
-        email = newsletter_form.cleaned_data.get("email")
-        if email:
-            messages.success(request, 'Successfully subscribed to our Newsletter!')
+    if request.method == 'POST':
+        if newsletter_form.is_valid():
+            email = newsletter_form.cleaned_data.get("email")
+            if email:
+                messages.success(request, 'Successfully subscribed to our Newsletter!')
+            else:
+                messages.error(request, 'This email already exists!')
+            newsletter_form.save()
         else:
             messages.error(request, 'This email already exists!')
-        newsletter_form.save()
-    else:
-        messages.error(request, 'This email already exists!')
-        newsletter_form = NewsletterForm()
+            newsletter_form = NewsletterForm()
 
     context = {
         'newsletter_form': NewsletterForm(),
