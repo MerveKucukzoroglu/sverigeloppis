4fe97fa5e9d92a6191f2af8368ad17f1bb4ac02a
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-19 11:51:34 +0000

    Add 404 page

--- sverige_loppis/urls.py
+++ sverige_loppis/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -29,3 +30,4 @@ urlpatterns = [
     path('subscription/', include('subscription.urls')),
     path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'sverige_loppis.views.handler404'
---
+++ sverige_loppis/views.py
@@ -0,0 +1,7 @@
+"""Views file for 404 page"""
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
