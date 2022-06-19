5edb9a2c176b483358bd33f28fcdd2d37a3075fb
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-19 12:24:50 +0000

    Connect Heroku for deployment

--- sverige_loppis/settings.py
+++ sverige_loppis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d56986h*fn1p34bd9@a7u5=#ui88v=hclyo#%sc+bz&tvvs)jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sverigeloppis.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'sverige_loppis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if  'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
