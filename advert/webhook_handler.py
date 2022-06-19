3699b2cd51a845ac891372db337b1e65223a8992
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-19 17:11:33 +0000

    Add advert.email to send real email to owner

--- advert/webhook_handler.py
+++ advert/webhook_handler.py
@@ -18,7 +18,7 @@ class StripeWH_Handler:
         """Send user confirmation email"""
         advert = Advert.objects.all()
         field_name = 'email'
-        user = User.objects.first()
+        user = advert.email
         loppis = Loppis.objects.first()
         field_value = getattr(user, field_name)
         seller_email = field_value
