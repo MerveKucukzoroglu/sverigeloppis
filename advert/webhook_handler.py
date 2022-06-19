d30187dcd55cc779d7268256556d431acaf97b43
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-06-19 18:18:10 +0000

    Remove sending multiple emails after stripe payment

--- advert/webhook_handler.py
+++ advert/webhook_handler.py
@@ -46,7 +46,7 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         advert = Advert.objects.all()
-        self._send_confirmation_email(advert)
+        # self._send_confirmation_email(advert)
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
@@ -60,7 +60,7 @@ class StripeWH_Handler:
             pid=intent.id
             stripe_pid=pid
             advert=Advert.objects.all()
-            self._send_confirmation_email(advert)
+            # self._send_confirmation_email(advert)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]}',
                 status=200)
@@ -68,11 +68,12 @@ class StripeWH_Handler:
         except Exception as e:
             if advert:
                 advert.delete()
-                self._send_confirmation_email(advert)
+                # self._send_confirmation_email(advert)
             return HttpResponse(
                 content=f'Webhook recieved: {event["type"]} | ERROR: {e}',
                 status=500
             )
+        self._send_confirmation_email(advert)
 
     def handle_payment_intent_payment_failed(self, event):
         """
